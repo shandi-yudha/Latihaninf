57fcdcdc9af2aa92c95eb276a7ddb3e1eeddee45
shandi-yudha <shandiyudha8@gmail.com>
2025-05-25 12:12:49 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,8 @@ import streamlit as st
 
 st.title("Selamat Datang di web Ini :)")
 st.write(
-    "Ngoding seru bersama shnydh"
+    "web kosong milik shnydh"
 )
 st.image("400px-Weather_Report_Stand_Infobox_Manga.png", width=200)
+st.write(
+    "stand di atas stand op bernama Weather Report"
